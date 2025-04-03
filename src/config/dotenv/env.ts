56dc2b2f97c7f1572f8a9f72ce64289a15e22916
GabrielLima2803 <gabriellima2803@gmail.com>
2025-04-02 23:16:09 -0300

    feat: Configurando a API key do Google

--- src/config/dotenv/env.ts
+++ src/config/dotenv/env.ts
@@ -4,7 +4,8 @@ const envScheme = z.object({
     PORT: z.coerce.number().default(3000),
     REDIS_URL: z.string().url(),
     DATABASE_URL: z.string().url(),
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev')
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
 })
 
 export const env = envScheme.parse(process.env)
