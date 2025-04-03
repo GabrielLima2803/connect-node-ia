12dd1e1b7016dc92e0fac7084703e5032a156194
GabrielLima2803 <gabriellima2803@gmail.com>
2025-04-02 23:13:03 -0300

    feat: Implementando rota no arquivo de rotas principal

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { rankingRouter } from "./ranking-router";
 import { inviteRouter } from "./invite-router";
 import { subscriptionRouter } from "./subscription-router";
+import { messageRouter } from "./message-ai-router";
 
 const routes = Router();
 
-routes.use( rankingRouter);
-routes.use( subscriptionRouter);
-routes.use( inviteRouter);
+routes.use(rankingRouter);
+routes.use(subscriptionRouter);
+routes.use(inviteRouter);
+routes.use(messageRouter)
 
 export {routes};
